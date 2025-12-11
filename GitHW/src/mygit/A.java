876bd3b9c86f4e49c65bb258ac8f2753ad0d3020
jangseohu <jangseohu@localhost>
2025-12-11 16:32:19 +0900

    Bob version

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -3,5 +3,6 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello this is A!");
+		System.out.println("Bob edited");
 	}
 }
--- GitHW/src/mygit/B.java
+++ GitHW/src/mygit/B.java
@@ -3,5 +3,6 @@ package mygit;
 public class B {
 	public static void main(String[] args) {
 		System.out.println("Hello BBBB!~~");
+		System.out.println("Bob edited");
 	}
 }
